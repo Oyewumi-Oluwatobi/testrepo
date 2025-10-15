1b216b10263260d6d527e0c0a964db42bba7c76c
Oyewumi-Oluwatobi <132371883+Oyewumi-Oluwatobi@users.noreply.github.com>
2025-10-15 02:11:27 +0100

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,32 @@
+"""
+def area(l,b):
+   return (0.5 * l * b)
+l = float(input('enter the height of the triangle= '))
+b = float(input('enter breadth of the triangle= '))
+result = area(l,b)
+print(f'The area of the triangle is {result}')
+"""
+'''num= int(input('enter any number: '))
+if num % 2 == 0:
+    print(f"{num} is an even number")
+else:
+    print(f"{num} is an odd number")'''
+
+'''import math
+def prime(x):
+    if x < 2:
+        return False
+    for i in range(2, int(math.sqrt(x))+1):
+        if x % i == 0:
+            return False
+    return True
+num= int(input('enter a number: '))
+if prime(num):
+    print ('it is a prime number')
+else:
+    print('it is not a prime number')
+'''
+'''print('even numbers between 1-60')
+for number in range (2, 61, 2):
+    print(number)'''
+
